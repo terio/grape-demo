110f019f5915f202ded9ede204988eba785e861c
Aditya Subramanyam <subramanyam963@gmail.com>
2019-02-23 22:31:39 +0530

    testing null components

--- client/components/app.js
+++ client/components/app.js
@@ -4,9 +4,11 @@ class List extends Component {
         this.setState({});
     }
     render() {
-        return <ul onclick={this.handleClick.bind(this)}>{this.props.custom.items.map(e => <li key={e}>{e}</li>)}</ul>;
+        // return <ul onclick={this.handleClick.bind(this)}>{this.props.custom.items.map(e => <Li key={e}>{e}</Li>)}</ul>;
+        return <ul onclick={this.handleClick.bind(this)}>{this.props.custom.items.map(e => <li onclick={this.props.custom.deleteItem.bind(null, e)} key={e}>{e}</li>)}</ul>;
     }
 }
+class Li extends Component {}
 export default class App extends Component {
     constructor(props, children) {
         super(props, children);
@@ -23,12 +25,20 @@ export default class App extends Component {
         });
     }
     remove() {
+        const newCounter = this.state.counter - 1;
         this.state.items.pop();
+        this.setState({
+            items: this.state.items,
+            counter: newCounter
+        });
+    }
+    deleteItem(num) {
+        this.state.items.splice(this.state.items.indexOf(num), 1);
         this.setState({
             items: this.state.items
         });
     }
     render() {
-        return <div>yo{this.state.counter % 2 ? <span>Yo man</span> : null}<button onclick={this.add.bind(this)}>add</button><button onclick={this.remove.bind(this)}>remove</button><div><List items={this.state.items}/></div></div>;
+        return <div>yo{this.state.counter % 2 ? <span>Yo man</span> : null}<button onclick={this.add.bind(this)}>add</button><button onclick={this.remove.bind(this)}>remove</button><div><List deleteItem={this.deleteItem.bind(this)} items={this.state.items}/></div></div>;
     }
 };
